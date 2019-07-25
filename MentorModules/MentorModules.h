43c28474ed056734ee04670b7509b054bdecea08
Jeffrey Wang <jeffrey.wang@edriving.com>
2019-07-25 15:01:52 -0700

    Initial Commit

---
+++ MentorModules/MentorModules.h
@@ -0,0 +1,19 @@
+//
+//  MentorModules.h
+//  MentorModules
+//
+//  Created by Jeffrey Wang on 7/25/19.
+//  Copyright © 2019 eDriving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MentorModules.
+FOUNDATION_EXPORT double MentorModulesVersionNumber;
+
+//! Project version string for MentorModules.
+FOUNDATION_EXPORT const unsigned char MentorModulesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MentorModules/PublicHeader.h>
+
+
